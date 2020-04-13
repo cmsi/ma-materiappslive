836f33d3f393cddd786c40a3c417fe0d35bbba3b
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2020-04-13 14:58:54 +0900

    Add test for compilers

--- files/test/test.sh
+++ files/test/test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# C, C++, Fortran, MPI
+
+cc --version > /dev/null 2>&1 || echo "Error: cc"
+c++ --version > /dev/null 2>&1 || echo "Error: c++"
+f95 --version > /dev/null 2>&1 || echo "Error: f95"
+mpicc --version > /dev/null 2>&1 || echo "Error: mpicc"
+mpic++ --version > /dev/null 2>&1 || echo "Error: mpic++"
+mpif90 --version > /dev/null 2>&1 || echo "Error: mpif90"
+
 # python
 
 python2 -c "import numpy; import scipy; import matplotlib; import sympy" || echo "Error: python2"
